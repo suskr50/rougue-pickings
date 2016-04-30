e40751162e04e054856684e06f3496a183af7bc1
Susan <susanschrum@mgail.com>
2016-04-30 00:06:09 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+
+$(window).resize(function() {
+   if ($(this).width() > 480) {
+
+   	alert ('Here I am');
+     
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+
+
+
+   }
+});
